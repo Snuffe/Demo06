f1d06293a1353e618f6e1cac8db4e970cdf2a1c3
Snuffe <K1493@student.jamk.fi>
2017-02-22 23:48:16 +0200

    Teht1

---
+++ Demo06/AS01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userData = "\n";
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"C:\Users\Käyttäjä\Source\Repos\Demo6_01\text.txt");
+            while (userData != "")
+            {
+                Console.Write("Give a text line (enter ends) : ");
+                userData = Console.ReadLine();
+                if (userData != "")
+                {
+                    outputFile.WriteLine(userData);
+                }
+            }
+            outputFile.Close();
+
+            var lines = File.ReadAllLines(@"C:\Users\Käyttäjä\Source\Repos\Demo6_01\text.txt");
+            Console.WriteLine("Contents of text.txt: ");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            outputFile.Close();
+        }
+    }
+}
